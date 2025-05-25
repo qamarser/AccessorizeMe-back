2610cd31a9bb60e3c43ce015120e5b52da67ee40
Qamar Serhan <serhanqamar@gmail.com>
2025-05-25 16:42:48 +0300

    update cors

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ||
-      "http://localhost:5173", 
+    origin: ["https://accessorize-me-front.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
